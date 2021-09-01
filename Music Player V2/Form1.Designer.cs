00620146ffd750771e4809d3776009e3fca2876d
abdel <abdel@DESKTOP-EOQEGOP>
2021-09-01 21:34:55 +0100

    Add project files.

---
+++ Music Player V2/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Music_Player_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectSongsBtn = new System.Windows.Forms.Button();
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.WindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMediaPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.minimizeBtn);
+            this.panel1.Controls.Add(this.closeBtn);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(968, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Image = ((System.Drawing.Image)(resources.GetObject("minimizeBtn.Image")));
+            this.minimizeBtn.Location = new System.Drawing.Point(874, 17);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Padding = new System.Windows.Forms.Padding(3);
+            this.minimizeBtn.Size = new System.Drawing.Size(38, 33);
+            this.minimizeBtn.TabIndex = 2;
+            this.minimizeBtn.UseVisualStyleBackColor = true;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Image = ((System.Drawing.Image)(resources.GetObject("closeBtn.Image")));
+            this.closeBtn.Location = new System.Drawing.Point(918, 17);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Padding = new System.Windows.Forms.Padding(3);
+            this.closeBtn.Size = new System.Drawing.Size(38, 33);
+            this.closeBtn.TabIndex = 1;
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(44, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ZORO Music PLayer";
+            // 
+            // selectSongsBtn
+            // 
+            this.selectSongsBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.selectSongsBtn.FlatAppearance.BorderSize = 0;
+            this.selectSongsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.selectSongsBtn.Font = new System.Drawing.Font("Roboto", 12F);
+            this.selectSongsBtn.ForeColor = System.Drawing.Color.White;
+            this.selectSongsBtn.Image = ((System.Drawing.Image)(resources.GetObject("selectSongsBtn.Image")));
+            this.selectSongsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.selectSongsBtn.Location = new System.Drawing.Point(12, 464);
+            this.selectSongsBtn.Name = "selectSongsBtn";
+            this.selectSongsBtn.Padding = new System.Windows.Forms.Padding(3);
+            this.selectSongsBtn.Size = new System.Drawing.Size(208, 43);
+            this.selectSongsBtn.TabIndex = 1;
+            this.selectSongsBtn.Text = "OPEN";
+            this.selectSongsBtn.UseVisualStyleBackColor = false;
+            this.selectSongsBtn.Click += new System.EventHandler(this.selectSongsBtn_Click);
+            // 
+            // listBoxSongs
+            // 
+            this.listBoxSongs.BackColor = System.Drawing.Color.DimGray;
+            this.listBoxSongs.Font = new System.Drawing.Font("Roboto", 11.25F);
+            this.listBoxSongs.ForeColor = System.Drawing.Color.White;
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.ItemHeight = 18;
+            this.listBoxSongs.Location = new System.Drawing.Point(12, 110);
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.Size = new System.Drawing.Size(208, 346);
+            this.listBoxSongs.TabIndex = 2;
+            this.listBoxSongs.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBoxSongs_DrawItem);
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(373, 522);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(10);
+            this.label2.Size = new System.Drawing.Size(240, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "              Made by Abdelatif Laghjaj ©2021";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // WindowsMediaPlayer
+            // 
+            this.WindowsMediaPlayer.Enabled = true;
+            this.WindowsMediaPlayer.Location = new System.Drawing.Point(226, 110);
+            this.WindowsMediaPlayer.Name = "WindowsMediaPlayer";
+            this.WindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WindowsMediaPlayer.OcxState")));
+            this.WindowsMediaPlayer.Size = new System.Drawing.Size(730, 397);
+            this.WindowsMediaPlayer.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.ForeColor = System.Drawing.Color.LightGreen;
+            this.label3.Location = new System.Drawing.Point(8, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(847, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PLease put all your songs in a separate folder and then  click on \"OPEN\" button, " +
+    "Then select all of the songs for best experience";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(968, 564);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.WindowsMediaPlayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxSongs);
+            this.Controls.Add(this.selectSongsBtn);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ZORO Music Player";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.WindowsMediaPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button minimizeBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button selectSongsBtn;
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.Label label2;
+        private AxWMPLib.AxWindowsMediaPlayer WindowsMediaPlayer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Music Player V2/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace Music_Player_V2
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        string[] paths, files;
+        string path;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void selectSongsBtn_Click(object sender, EventArgs e)
+        {
+            WMPLib.IWMPPlaylist playlist = WindowsMediaPlayer.playlistCollection.newPlaylist("myplaylist");
+            WMPLib.IWMPMedia media;
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Multiselect = true;
+            file.InitialDirectory = "Desktop\\";
+            file.Filter = "mp3 files (*.mp3)|*.mp3 | wav files (*.wav)|*.wav | All files (*.*)|*.*";
+            file.FilterIndex = 3;
+            file.RestoreDirectory = true;
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                files = file.SafeFileNames;
+                paths = file.FileNames;
+                path = Path.GetDirectoryName(file.FileName);
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBoxSongs.Items.Add(files[i]);
+                }
+
+                
+                foreach (string item in file.FileNames)
+                {
+                    media = WindowsMediaPlayer.newMedia(item);
+                    playlist.appendItem(media);
+                }
+
+                WindowsMediaPlayer.currentPlaylist = playlist;
+                WindowsMediaPlayer.Ctlcontrols.play();
+            }
+        }
+
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxSongs != null && listBoxSongs.Items != null)
+            {
+                WindowsMediaPlayer.URL = paths[listBoxSongs.SelectedIndex];
+            }
+        }
+
+        private void listBoxSongs_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            string selectedItem = listBoxSongs.Items[e.Index].ToString();
+
+            // 2. Choose font 
+            Font font = new Font("Roboto", 30);
+
+            // 3. Choose colour
+            SolidBrush solidBrush = new SolidBrush(Color.Red);
+
+            // 4. Get bounds
+            int left = e.Bounds.Left;
+            int top = e.Bounds.Top;
+
+            // 5. Use Draw the background within the bounds
+            e.DrawBackground();
+
+            // 6. Colorize listbox items
+            e.Graphics.DrawString(selectedItem, font, solidBrush, left, top);
+        }
+
+        private void minimizeBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
